0a0b0b41ab2ec272d433fbb08a672fe93baa31ec
Chronicle20 <a.chronicle.20@gmail.com>
2024-08-19 08:41:21 -0400

    Add support for returning cash equipment.

--- atlas.com/character/equipment/slot/rest.go
+++ atlas.com/character/equipment/slot/rest.go
@@ -5,12 +5,14 @@ import (
 )
 
 type RestModel struct {
-	Position  Position             `json:"position"`
-	Equipable *equipable.RestModel `json:"equipable"`
+	Position      Position             `json:"position"`
+	Equipable     *equipable.RestModel `json:"equipable"`
+	CashEquipable *equipable.RestModel `json:"cashEquipable"`
 }
 
 func Transform(model Model) (RestModel, error) {
 	var rem *equipable.RestModel
+	var rcem *equipable.RestModel
 	if model.Equipable != nil {
 		m, err := equipable.Transform(*model.Equipable)
 		if err != nil {
@@ -18,10 +20,18 @@ func Transform(model Model) (RestModel, error) {
 		}
 		rem = &m
 	}
+	if model.CashEquipable != nil {
+		m, err := equipable.Transform(*model.CashEquipable)
+		if err != nil {
+			return RestModel{}, err
+		}
+		rcem = &m
+	}
 
 	rm := RestModel{
-		Position:  model.Position,
-		Equipable: rem,
+		Position:      model.Position,
+		Equipable:     rem,
+		CashEquipable: rcem,
 	}
 	return rm, nil
 }
@@ -34,6 +44,13 @@ func Extract(model RestModel) (Model, error) {
 			return m, err
 		}
 		m.Equipable = &e
+	}
+	if model.CashEquipable != nil {
+		e, err := equipable.Extract(*model.CashEquipable)
+		if err != nil {
+			return m, err
+		}
+		m.CashEquipable = &e
 	}
 	return m, nil
 }
